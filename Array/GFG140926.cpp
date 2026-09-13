1e3e3829e1df687248610fd1449d1727ed06bc52
vivekkr-05 <kumarvivekrachna0007@gmail.com>
2026-09-14 01:14:45 +0530

    14 Sep

---
+++ Array/GFG140926.cpp
@@ -0,0 +1,17 @@
+// Given an array arr[], the task is to find whether the arr is palindrome or not.  An array is said to be palindrome if its reverse array matches the original array.
+
+
+class Solution {
+  public:
+    bool isPalindrome(vector<int> &arr) {
+        // code here
+        int n=(1+arr.size())/2;
+        int a=arr.size();
+        for(int i=0; i<=n; i++){
+            if(arr[i]!=arr[a-1])
+            return false;
+            a--;
+        }
+        return true;
+    }
+};
